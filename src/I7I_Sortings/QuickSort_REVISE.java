a9b8556da9529b69bd96193cfb4b9b07c07fc682
Billy <shantanupawar292@gmail.com>
2023-09-10 13:24:21 +0530

    add quicksort

--- src/I7I_Sortings/QuickSort_REVISE.java
+++ src/I7I_Sortings/QuickSort_REVISE.java
@@ -12,8 +12,10 @@ It has a worst-case time complexity of O(N2), which occurs when the pivot is cho
 It is not a good choice for small data sets.
  */
 
-import java.util.Arrays;
 
+//GFG Ques link : https://practice.geeksforgeeks.org/problems/quick-sort/1
+
+import java.util.Arrays;
 public class QuickSort_REVISE {
     static void swap(int[] arr, int i, int j) {
         int temp = arr[i];
@@ -23,36 +25,23 @@ public class QuickSort_REVISE {
 
     static int partition(int[] arr, int low, int high) {
         int pivot = arr[high];
-        int i = (low - 1);
+        int pIndex = low;
 
-        for (int j = low; j <= high - 1; j++) {
-
-            // If current element is smaller than the pivot
-            if (arr[j] < pivot) {
-
-                // Increment index of smaller element
-                i++;
-                swap(arr, i, j);
+        for (int i = low; i < high ; i++) {
+            if(arr[i] <= pivot){
+                swap(arr, i, pIndex);
+                pIndex++;
             }
         }
-        swap(arr, i + 1, high);
-        return (i + 1);
+
+        swap(arr, pIndex, high);
+        return pIndex;
     }
 
-    // The main function that implements QuickSort
-    // arr[] --> Array to be sorted,
-    // low --> Starting index,
-    // high --> Ending index
-    static void quickSort(int[] arr, int low, int high)
-    {
-        if (low < high) {
+    static void quickSort(int[] arr, int low, int high){
 
-            // pi is partitioning index, arr[p]
-            // is now at right place
+        if (low < high) { // pi = Pindex [partition index]
             int pi = partition(arr, low, high);
-
-            // Separately sort elements before
-            // partition and after partition
             quickSort(arr, low, pi - 1);
             quickSort(arr, pi + 1, high);
         }
@@ -60,7 +49,6 @@ public class QuickSort_REVISE {
 
     public static void main(String[] args){
         int[] arr = { 10, 7, 8, 9, 1, 5 };
-
         quickSort(arr, 0, arr.length - 1);
         System.out.println(Arrays.toString(arr));
     }
